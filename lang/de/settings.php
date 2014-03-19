bb415c26fb8a9ab1f61f6b6f9cc0f88a63ac1edf
BlackRocket <sebastian@brcs.eu>
2014-03-19 19:56:17 +0100

    upgrade lang and add german lang

---
+++ lang/de/settings.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * english language file for shorty plugin
+ *
+ * @author Fernando Ribeiro <pinguim.ribeiro@gmail.com>
+ */
+
+// keys need to match the config setting name
+
+$lang['bitly_oauth_api']            = 'Die URI zum Endpunkt der bitly OAuth API.<br>(Standard ist https://api-ssl.bitly.com/v3/).<br>Dies ist gültig für die Dienste bit.ly, bitly.com und j.mp.';
+$lang['bitly_oauth_access_token']   = 'Dein OAuth Zugriffsschlüssel<br>(https://bitly.com/a/oauth_apps).<br>Dies ist gültig für die Dienste bit.ly, bitly.com und j.mp.';
+$lang['yourls_url']            		= 'Die URI zu deiner yourls Installation.';
+$lang['yourls_token']   			= 'Deine geheimer Signaturschlüssel.';
+$lang['default_service']            = 'Der Standarddienst';
--- lang/en/settings.php
+++ lang/en/settings.php
@@ -9,5 +9,6 @@
 
 $lang['bitly_oauth_api']            = 'The URI of the bitly OAuth endpoints.<br>(Default is https://api-ssl.bitly.com/v3/).<br>This is valid for bit.ly, bitly.com and j.mp services';
 $lang['bitly_oauth_access_token']   = 'Your oauth Access Token<br>(https://bitly.com/a/oauth_apps).<br>This is valid for bit.ly, bitly.com and j.mp services.';
-
+$lang['yourls_url']            		= 'The URI of the yourls installation (without trailing slash)';
+$lang['yourls_token']   			= 'Your secret signature token.';
 $lang['default_service']            = 'The default service.';
